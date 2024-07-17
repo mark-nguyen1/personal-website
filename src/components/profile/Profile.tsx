946ac235a96e7c53462535d60ef9acb4020471b8
Mark Nguyen <mark_nguyen@berkeley.edu>
2024-07-17 00:54:46 -0700

    profile page

---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,26 @@
+import profilePicutre from "../../assets/profile-picture.png";
+const Profile = () => {
+  return (
+    <div className="profile-container">
+      <div className="content-container">
+        <h1>
+          Hello, I'm <br />
+          <span>Mark Nguyen</span>
+        </h1>
+        <p>
+          A computer science student from United States. I aim to deepen my
+          understanding in various areas of technology and product development.
+        </p>
+      </div>
+      <div className="image-container ">
+        <img
+          className="circular-image"
+          src={profilePicutre}
+          alt="my profile picture"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
