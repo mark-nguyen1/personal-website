c135a65f1e7be5b72be3e710b6fccf33050fa270
Mark Nguyen <mark_nguyen@berkeley.edu>
2024-07-17 14:25:08 -0700

    project card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import NavBar from "./components/navigation-bar/NavBar";
 import Profile from "./components/profile/Profile";
+import InfoCard from "./components/card/InfoCard";
+import projectData from "./data/projects.json";
 import "./App.css";
 
 function App() {
@@ -8,7 +10,14 @@ function App() {
     <>
       <NavBar></NavBar>
       <Profile></Profile>
-      <div id="main"></div>
+      {projectData.projects.map((project, index) => (
+        <InfoCard
+          img={project.img}
+          title={project.title}
+          techStacks={project.techStacks}
+          projectInfo={project.projectInfo}
+        ></InfoCard>
+      ))}
     </>
   );
 }
---
+++ src/components/card/InfoCard.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  img: string;
+  title: string;
+  techStacks: string;
+  projectInfo: string;
+}
+
+const InfoCard = ({ img, title, techStacks, projectInfo }: Props) => {
+  return (
+    <div className="card-container">
+      <img src={img}></img>
+      <div className="card-text-container ">
+        <h1>{title}</h1>
+        <h3>{techStacks}</h3>
+        <p>{projectInfo}</p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
