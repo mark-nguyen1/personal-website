df48c8fcdce77280f7b3e9d54858718e0ebd3caf
Mark Nguyen <mark_nguyen@berkeley.edu>
2024-07-17 00:54:24 -0700

    navbar template

---
+++ src/components/navigation-bar/NavBar.tsx
@@ -0,0 +1,18 @@
+const NavBar = () => {
+  return (
+    <>
+      {" "}
+      <div id="navBar" className="header">
+        <header>
+          <nav className="navbar">
+            <a href="#">home</a>
+            <a href="#">projects</a>
+            <a href="#">contact</a>
+          </nav>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
