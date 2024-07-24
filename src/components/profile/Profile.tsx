ce335eebee0a48b138cc54af10025d75e6a1c057
Mark Nguyen <mark_nguyen@berkeley.edu>
2024-07-24 01:28:21 -0700

    responsive profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -4,22 +4,22 @@ import "./profile.css";
 const Profile = () => {
   return (
     <div className="profile-container">
-      <div className="content-container">
-        <h1>
-          Hello, I'm <br />
-          <span>Mark Nguyen</span>
-        </h1>
-        <p>
-          A computer science student from United States. I aim to deepen my
-          understanding in various areas of technology and product development.
-        </p>
-      </div>
       <div className="image-container ">
         <img
           className="circular-image"
           src={profilePicutre}
           alt="my profile picture"
         ></img>
+      </div>
+      <div className="content-container">
+        <h1>
+          Hello, I'm <br />
+          <span>Mark Nguyen</span>
+        </h1>
+        <h2>
+          A computer science student from United States. I aim to deepen my
+          understanding in various areas of technology and product development.
+        </h2>
       </div>
     </div>
   );
